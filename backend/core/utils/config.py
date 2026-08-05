97671be8e9c2c6b0579559320b1ef502bc84c82f
Yao0454 <yaoyifeng20070524@outlook.com>
2026-08-05 04:12:19 +0000

    update

--- backend/core/utils/config.py
+++ backend/core/utils/config.py
@@ -8,14 +8,14 @@ from vllm.model_executor.layers.quantization import QuantizationMethods
 DEBUG_MODE: bool = True
 
 # model
-MODEL_PATH: str = "/remote_dir/home/chenxuzhao/models/Qwen3.5-9B"
+MODEL_PATH: str = "/remote_dir/home/chenxuzhao/models/Qwen3.5-122B-A10B"
 MODEL_DTYPE: ModelDType = "bfloat16"
 MODEL_KV_CACHE_DTYPE: CacheDType = "auto"
 MODEL_GPU_MEMORY_UTILIZATION: float = 0.95
-MODEL_MAX_MODEL_LENGTH: int = 65536
-MODEL_MAX_NUM_SEQS: int = 4
+MODEL_MAX_MODEL_LENGTH: int = 40960
+MODEL_MAX_NUM_SEQS: int = 16
 MODEL_QUANTIZATION: QuantizationMethods | None = None
-MODEL_TENSOR_PARALLEL_SIZE: int = 2
+MODEL_TENSOR_PARALLEL_SIZE: int = 4
 
 # agent
 AGENT_LOOP_TIME: int = 10
